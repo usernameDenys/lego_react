a12b1860fc72acaa8a841662ae61b1e5459c77f5
Denys <denysgolenko@gmail.com>
2025-04-22 12:06:21 +0200

    installed dependencies, routs, pages

---
+++ src/components/Card.jsx
@@ -0,0 +1,6 @@
+
+const Card = () => {
+    return <h1 className="text-center">Card</h1>
+}
+
+export default Card;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Card from "../components/Card";
+
+const HomePage = () => (
+
+    <div className='container mx-auto px-4'>
+
+        <Card />
+
+    </div>
+);
+
+export default HomePage;
